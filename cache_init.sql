3e6e918374f183bfdf30dbf2e3813044278414f2
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-10-14 08:54:36 +0200

    Added AGPL3+ notice in all source files

--- cache_init.sql
+++ cache_init.sql
@@ -1,3 +1,21 @@
+--  Script to initialize required tables for the PostgreSQL cache
+--  This file is part of dilbert-viewer.
+--
+--  Copyright (C) 2022  Harish Rajagopal <harish.rajagopals@gmail.com>
+--
+--  dilbert-viewer is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU Affero General Public License as published
+--  by the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  dilbert-viewer is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU Affero General Public License for more details.
+--
+--  You should have received a copy of the GNU Affero General Public License
+--  along with dilbert-viewer.  If not, see <https://www.gnu.org/licenses/>.
+
 -- Cache is used to avoid scraping every time
 CREATE TABLE IF NOT EXISTS comic_cache (
 	comic DATE NOT NULL, -- date of the comic
