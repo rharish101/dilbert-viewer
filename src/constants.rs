c27118ebbad6b8a33147debabf49582e156f961e
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-11-24 16:07:30 +0100

    Added sanity-check tests for constants

--- src/constants.rs
+++ src/constants.rs
@@ -67,3 +67,70 @@ pub const CSP: &str = "\
     default-src 'none';\
     img-src 'self' assets.amuniversal.com;\
     style-src 'self' cdn.jsdelivr.net";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::path::Path;
+
+    use actix_web::middleware::DefaultHeaders;
+    use chrono::NaiveDate;
+    use content_security_policy as csp;
+
+    #[test]
+    /// Test whether the first comic date is in the expected format.
+    fn test_first_comic_format() {
+        assert!(
+            NaiveDate::parse_from_str(FIRST_COMIC, SRC_DATE_FMT).is_ok(),
+            "FIRST_COMIC doesn't match SRC_DATE_FMT"
+        )
+    }
+
+    #[test]
+    /// Test whether the date format for "dilbert.com" is valid.
+    fn test_src_date_format() {
+        NaiveDate::from_ymd_opt(2000, 1, 1)
+            .unwrap()
+            .format(SRC_DATE_FMT)
+            // This should panic at `.to_string` if the format is invalid.
+            .to_string();
+    }
+
+    #[test]
+    /// Test whether the date format used for displaying is valid.
+    fn test_disp_date_format() {
+        NaiveDate::from_ymd_opt(2000, 1, 1)
+            .unwrap()
+            .format(DISP_DATE_FMT)
+            // This should panic at `.to_string` if the format is invalid.
+            .to_string();
+    }
+
+    #[test]
+    /// Test whether the directory of static files exists.
+    fn test_if_static_dir_exists() {
+        assert!(
+            Path::new(STATIC_DIR).exists(),
+            "Static directory doesn't exist"
+        );
+    }
+
+    #[test]
+    /// Test whether the content security policy (CSP) is a valid header value.
+    ///
+    /// Note that this doesn't check if the CSP follows the CSP format.
+    fn test_content_security_policy_header_format() {
+        // This panics if the *header* format is invalid (not CSP format).
+        DefaultHeaders::new().add(("Content-Security-Policy", CSP));
+
+        let policy = csp::Policy::parse(
+            CSP,
+            csp::PolicySource::Header,
+            csp::PolicyDisposition::Enforce,
+        );
+        assert!(policy.is_valid(), "CSP is invalid");
+        // See if at least one directive exists.
+        assert!(!policy.directive_set.is_empty(), "CSP has no directives");
+    }
+}
