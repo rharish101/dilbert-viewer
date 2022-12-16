69119de86db7035b7650569f80f0ac26f49ae241
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-12-16 13:08:15 +0100

    Default log level is now warn, not error

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,15 @@ use std::str::FromStr;
 
 use portpicker::{is_free, pick_unused_port};
 use tracing::error;
-use tracing_subscriber::EnvFilter;
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 /// Default port when one isn't specified
 // This is Heroku's default port when running locally
 const PORT: u16 = 5000;
+
+/// Default log level
+const LOG_LEVEL: LevelFilter = LevelFilter::WARN;
 
 // Environment variables that are read
 /// Port on which to run the server
@@ -33,15 +37,33 @@ const PORT_VAR: &str = "PORT";
 /// Redis database connection URL
 const REDIS_URL_VAR: &str = "REDIS_TLS_URL";
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+/// Initialize the logger from the `RUST_LOG` environment variable, with a default.
+fn init_logger() -> WorkerGuard {
     // Log to stdout in a non-blocking way using a logging thread.
-    let (writer, _guard) = tracing_appender::non_blocking(stdout());
+    let (writer, guard) = tracing_appender::non_blocking(stdout());
+
+    // Use the `RUST_LOG` env var, like `env_logger`, but with a default.
+    let builder = EnvFilter::builder().with_default_directive(LOG_LEVEL.into());
+    let filter = match builder.try_from_env() {
+        Ok(filter) => filter,
+        Err(err) => {
+            println!("Invalid log level: {err}");
+            builder.parse_lossy("")
+        }
+    };
+
     tracing_subscriber::fmt()
-        // Use the `RUST_LOG` env var, like `env_logger`.
-        .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(filter)
         .with_writer(writer)
         .init();
+
+    guard
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // The non-blocking writer stays active as long as `_guard` is not dropped.
+    let _guard = init_logger();
 
     let port = if let Some(port) = env::var(PORT_VAR)
         .ok()
